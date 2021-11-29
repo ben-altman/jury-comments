ff58cf463cca12db45a7ae068e160db4a6d98d1b
github username <github email address>
2021-11-28 23:37:27 -0500

    create nested routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :repertoires
-  resources :comments
-  resources :juries
+  resources :juries do
+    resources :repertoires
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
