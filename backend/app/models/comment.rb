213e5b31f1b781ce44929d0c19ef35ec0397cd28
github username <github email address>
2021-11-30 13:06:54 -0500

    comment model validate score

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
   validates :content, presence: :true
-  
+  validates :score, presence: :true
+  validates :score, numericality: { in: 0..10 }
+
   belongs_to :jury
 end
