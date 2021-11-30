fcb8aa805e9600e093b16cc2255615b9bc9c8410
github username <github email address>
2021-11-30 12:50:54 -0500

    add nested attributes to jury model

--- backend/app/models/jury.rb
+++ backend/app/models/jury.rb
@@ -1,4 +1,8 @@
 class Jury < ApplicationRecord
     has_many :comments
     has_many :repertoires
+
+    accepts_nested_attributes_for :comment, :reject_if => proc { |a| a[:content].blank? }
+    accepts_nested_attributes_for :repertore, :reject_if => proc { |a| a[:title].blank? }
+
 end
--- backend/app/models/repertoire.rb
+++ backend/app/models/repertoire.rb
@@ -1,5 +1,4 @@
 class Repertoire < ApplicationRecord
   belongs_to :jury
 
-  accepts_nested_attributes_for :jury, :reject_if => proc { |a| a[:name].blank? }
 end
