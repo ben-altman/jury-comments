a272065bbdc75302796c8c91f4206c0ac38e87e2
github username <github email address>
2021-12-01 23:01:52 -0500

    more frontend setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,10 @@
 // placeholder here 
 
-fetch("http://127.0.0.1:3000")
+// fetch("http://127.0.0.1:3000")
+
+console.log('testing...')
+
+const BACKEND_URL = 'http://127.0.0.1:3000';
+fetch(`${BACKEND_URL}/test`)
+    .then(response => response.json())
+    .then(parsedResponse => console.log(parsedResponse));
