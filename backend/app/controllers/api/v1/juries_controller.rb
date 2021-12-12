e1e0190d7a01f87c338c94c2171d0866ca2e89de
github username <github email address>
2021-12-11 23:25:23 -0500

    build jury index and create methods

--- backend/app/controllers/api/v1/juries_controller.rb
+++ backend/app/controllers/api/v1/juries_controller.rb
@@ -4,6 +4,15 @@ class Api::V1::JuriesController < ApplicationController
         render json: juries
     end
 
+    def create
+        jury = Jury.new(jury_params)
+        if jury.save
+            render json: jury, status: :accepted
+        else
+            render json: {errors: jury.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
     # def show
     #     jury = Jury.find(params[:id])
     #     render json: jury
@@ -12,4 +21,10 @@ class Api::V1::JuriesController < ApplicationController
     # def create
         
     # end
+
+    private
+
+    def jury_params
+        params.require(:jury).permit(:name, :instrument, :technique)
+    end
 end
