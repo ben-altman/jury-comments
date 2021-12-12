bc69a568b102ac8ed2af342270f9a465abe68b10
github username <github email address>
2021-12-12 00:11:18 -0500

    fix typo

--- backend/app/serializers/repertoire_serializer.rb
+++ backend/app/serializers/repertoire_serializer.rb
@@ -1,5 +1,5 @@
 class RepertoireSerializer < ActiveModel::Serializer
   attributes :id, :composer, :title
 
-  belongs_to :jury, serializer: JurySerializer
+  belongs_to :jury, #serializer: JurySerializer
 end
