ef75a767d1656e9d0bba0241b9d3316b06e07524
github username <github email address>
2021-12-13 22:10:06 -0500

    first fetch of all juries

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,28 @@
+const endPoint = "http://localhost:3000/api/v1/juries"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getJuries()
+})
+
+function getJuries() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(juries => {
+        juries.forEach(jury => {
+            // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+            // debugger
+            const juryMarkup = `
+                <div data-id=${jury.id}>
+                    <h3>${jury.name}</h3>
+                    <p>${jury.instrument}</p>
+                    <button data-id=${jury.id}>edit</button>
+                </div>
+                </br></br>`
+      
+            document.querySelector('#jury-container').innerHTML += juryMarkup
+          })
+    })
+}
 // const BACKEND_URL = 'http://127.0.0.1:3000';
 // fetch(`${BACKEND_URL}/test`)
 //     .then(response => response.json())
@@ -7,3 +32,4 @@
 const
 fetch("http://127.0.0.1:3000")
 */
+
