641318b470d8fd40fb1bb8fe048e46952760ac70
github username <github email address>
2021-12-19 23:08:00 -0500

    building oo js jury class

--- frontend/src/jury.js
+++ frontend/src/jury.js
@@ -1,2 +1,15 @@
 const baseURL = "http://localhost:3000"
 const juriesList = document.querySelector('#jury-container')
+
+class Jury {
+    static all = []
+
+    constructor(id, name, instrument, technique) {
+        this.id = id
+        this.name = name
+        this.instrument = instrument
+        this.technique = technique
+        Jury.all.push(this)
+    }
+
+}
